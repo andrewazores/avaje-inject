453b65766efe7399c8d7bbc8370e24cefbae7fac
rob <robin.bygrave@gmail.com>
2023-02-21 09:17:29 +1300

    Update test for javax

--- inject-test/src/test/java/org/example/generic/EventBusImpl.java
+++ inject-test/src/test/java/org/example/generic/EventBusImpl.java
@@ -1,6 +1,6 @@
 package org.example.generic;
 
-import jakarta.inject.Singleton;
+import javax.inject.Singleton;
 
 @Singleton
 public class EventBusImpl implements EventBus<Object, Subscriber<?>> {
